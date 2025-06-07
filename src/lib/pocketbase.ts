d3efe7be3f695982efc9e0cba1a6fd1a58534e0b
mamadgmu <mamadgmyou4@gmail.com>
2025-06-07 21:35:34 +0330

    made public

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -4,4 +4,4 @@ import PocketBase from 'pocketbase';
 const POCKETBASE_HOST = 'https://apihezartoo.mammutepd.ir/';
 const LOCAL_HOST = 'http://127.0.0.1:8090/';
 
-export const pb = new PocketBase(LOCAL_HOST);
+export const pb = new PocketBase(POCKETBASE_HOST);
